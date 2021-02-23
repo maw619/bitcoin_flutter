cdb7fac3cb1aecef670d7e2c566d2065c9913d39
Marco Wolff <marcowolff619@gmail.com>
2021-02-23 18:30:09 -0500

    add working API calls from url

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -18,14 +20,15 @@ class _MyAppState extends State<MyApp> {
   var priceUsd;
 
   Future getCrypto() async {
-    http.Response response = await http.get("api.coincap.io/v2/rates/bitcoin");
+    http.Response response =
+        await http.get("https://api.coincap.io/v2/assets/bitcoin");
     var results = jsonDecode(response.body);
 
     setState(() {
-      this.rank;
       this.coinSymbol = results['data']['symbol'];
-      this.name;
-      this.priceUsd;
+      this.rank = results['data']['rank'];
+      this.name = results['data']['name'];
+      this.priceUsd = results['data']['priceUsd'];
     });
   }
 
@@ -38,14 +41,41 @@ class _MyAppState extends State<MyApp> {
 //temp != null ? temp.toString() + "\u00B0" : "Loading",
   @override
   Widget build(BuildContext context) {
+    const PrimaryColor = const Color(0xFF000000);
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: Text('Crypto Ticker')),
-          body: Column(
-            children: <Widget>[
-              Text(coinSymbol != null ? coinSymbol.toString() : "na"),
-            ],
-          )),
-    );
+        theme: ThemeData(
+          primaryColor: PrimaryColor,
+        ),
+        home: Scaffold(
+            //backgroundColor: Colors.black,
+            //appBar: AppBar(title: Text('Crypto Ticker')),
+            body: SafeArea(
+          child: Container(
+              width: 600.00,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage('images/holly.png'),
+                fit: BoxFit.cover,
+              )),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: new Text(coinSymbol != null
+                        ? coinSymbol.toString()
+                        : "Loading..."),
+                  ),
+                  Container(
+                    child: Text(name != null ? name.toString() : "Loading..."),
+                  ),
+                  Container(
+                      child:
+                          Text(rank != null ? rank.toString() : "Loading...")),
+                  Container(
+                      child: Text(priceUsd != null
+                          ? priceUsd.toString()
+                          : "Loading...")),
+                ],
+              )),
+        )));
   }
 }
